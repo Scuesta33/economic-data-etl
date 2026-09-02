4686fdd253f0ac5b6d11c8428144132bca7ae632
Sergio Cuesta <scuesta33@gmail.com>
2026-09-02 20:06:37 +0200

    creacion de csv

--- src/salmon_etl.py
+++ src/salmon_etl.py
@@ -38,3 +38,46 @@ def limpiar_registro(registro):
         "calidad": str(quality)
     }
     return resultado, None
+def transformar_datos(datos):
+    datos_limpios = []
+    datos_rechazados = []
+    for registro in datos:
+        limpio, error = limpiar_registro(registro)
+
+        if limpio is None:
+            print("Registro rechazado:", registro)
+            print("Motivo:", error)
+
+            rechazo = {
+                "plant": registro.get("plant"),
+                "date": registro.get("date"),
+                "tons": registro.get("tons"),
+                "quality": registro.get("quality"),
+                "error": error
+            }
+            datos_rechazados.append(rechazo)
+            continue
+        datos_limpios.append(limpio)
+    return datos_limpios, datos_rechazados
+
+def guardar_salmon_csv(datos):
+
+    columnas = ["planta", "fecha", "toneladas", "calidad"]
+    with open("data/processed/salmon_clean.csv", "w", newline="", encoding="utf-8") as archivo:
+            writer = csv.DictWriter(archivo, fieldnames=columnas)
+            writer.writeheader()
+            writer.writerows(datos)
+
+def guardar_salmon_rechazados(datos):
+    columnas = ["plant", "date", "tons", "quality", "error"]
+    with open("data/processed/salmon_rejected.csv", "w", newline="", encoding="utf-8") as archivo:
+            writer = csv.DictWriter(archivo, fieldnames=columnas)
+            writer.writeheader()
+            writer.writerows(datos)
+
+datos = extraer_datos()
+datos_limpios, datos_rechazados = transformar_datos(datos)
+print(datos_limpios)
+print(datos_rechazados)
+guardar_salmon_csv(datos_limpios)
+guardar_salmon_rechazados(datos_rechazados)
